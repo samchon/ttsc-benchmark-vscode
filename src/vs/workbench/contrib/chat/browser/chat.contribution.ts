c468d91f40dcaf2aaae463dd4e5535affed97e26
bench <bench@local>
2026-05-27 10:36:43 +0900

    fix(benchmark): restore src/ + package.json from ttsc branch for ttsc-lint

---
+++ src/vs/workbench/contrib/chat/browser/chat.contribution.ts
@@ -0,0 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { registerAction2 } from '../../../../platform/actions/common/actions.js';
+import { ForkConversationAction } from './actions/chatForkActions.js';
+
+registerAction2(ForkConversationAction);
